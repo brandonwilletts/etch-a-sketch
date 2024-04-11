63031b55ac223e67d1923d774b0e2dd905d337fb
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2024-04-11 15:17:17 -0700

    Add button to set grid size
    Add button that sets # of squares per side.
    Create function to delete existing grid rows.
    Need to modify code to set limit to 100 squares per side.

--- javascript.js
+++ javascript.js
@@ -6,6 +6,19 @@ grid.addEventListener("mouseover", (event) => {
     target.style.opacity = 0.2;
 })
 
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    const squaresPerSide = prompt("Input # of squares per side (max 100)");
+    emptyGrid(document.querySelectorAll(".gridRow"));
+    drawSquares(squaresPerSide);
+})
+
+function emptyGrid (gridRows) {
+    for (let i = 0; i < gridRows.length; i++){
+        grid.removeChild(gridRows[i]);
+    } 
+}
+
 function drawSquares (squaresPerSide) {
     for (let i = 1; i <= squaresPerSide; i++) {
         const gridRow = document.createElement("div");
